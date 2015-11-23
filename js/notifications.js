f4e79ab026aa574b1b2c1e22298ba9aba5ce4690
Etienne Segonzac <etienne@segonzac.info>
2015-11-23 12:44:05 +0100

    Basic utility tray styling.

--- js/notifications.js
+++ js/notifications.js
@@ -3,7 +3,9 @@ window.addEventListener('DOMContentLoaded', function() {
   for (var i = 1; i <= 42; i++) {
     content.push({
       title: 'Notification number ' + i,
-      body: 'Lorem ipsum tralala'
+      body: 'Lorem ipsum tralala',
+      icon: `notification-icon-${parseInt(Math.random() * 10 % 8)}.png`,
+      time: `${parseInt(Math.random() * 100 % 12)}:${parseInt(Math.random() * 100 % 60)} ${(Math.random() > 0.5) ? 'a' : 'p'}`
     });
   }
 
@@ -12,8 +14,10 @@ window.addEventListener('DOMContentLoaded', function() {
     var notification = document.createElement('li');
 
     notification.innerHTML = `
+      <img src="assets/${c.icon}" />
       <h3>${c.title}</h3>
       <p>${c.body}</p>
+      <span>${c.time}</span>
     `;
 
     container.appendChild(notification);

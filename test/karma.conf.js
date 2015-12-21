1db797b4def2f463eddf72b23131ea74a9c2258b
Etienne Segonzac <etienne@segonzac.info>
2015-12-21 12:54:04 +0100

    Setting up the test and lint infrastructure.

---
+++ test/karma.conf.js
@@ -0,0 +1,56 @@
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '..',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: [
+      'mocha',
+      'chai-sinon'
+    ],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'node_modules/dom-scheduler/dom-scheduler.js',
+      'lib/**/*.js',
+      'test/**/*-test.js'
+    ],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: [
+      'progress'
+    ],
+
+    client: {
+      captureConsole: true,
+      mocha: { 'ui': 'tdd' }
+    },
+
+    // enable / disable watching file and
+    // executing tests whenever any file changes
+    autoWatch: true,
+
+    customLaunchers: {
+      firefox_latest: {
+        base: 'FirefoxNightly',
+        prefs: {
+          'dom.webcomponents.enabled': true,
+          'dom.w3c_touch_events.enabled': 1
+        }
+      }
+    },
+
+    // start these browsers
+    // available browser launchers:
+    // https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['firefox_latest'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
